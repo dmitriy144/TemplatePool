fa92bf1362d5def32ef7c718602431a9927aa8cf
Dmitriy Kilchitsky <dmitriy.kilchitsky@gmail.com>
2021-06-20 12:21:33 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'template-pool';
-}
+export class AppComponent {}
